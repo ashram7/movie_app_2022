6c6684496b82b790494222d1713b38933084fee5
ashram7 <ashram7@hanmail.net>
2022-03-03 17:55:50 +0900

    Updates

---
+++ precache-manifest.21cc1dbe08f65d2c66d3c55bd1eab17e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91331e669f9276c5298fb8e68ba37397",
+    "url": "/movie_app_2022/index.html"
+  },
+  {
+    "revision": "93dce44f96946e9323ea",
+    "url": "/movie_app_2022/static/css/main.3bc3c130.chunk.css"
+  },
+  {
+    "revision": "e51bd85f02d91c012ebc",
+    "url": "/movie_app_2022/static/js/2.679877bb.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/movie_app_2022/static/js/2.679877bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93dce44f96946e9323ea",
+    "url": "/movie_app_2022/static/js/main.a9061e2c.chunk.js"
+  },
+  {
+    "revision": "b4bd60b86ff74e150aaf",
+    "url": "/movie_app_2022/static/js/runtime-main.e76b7f5b.js"
+  }
+]);
